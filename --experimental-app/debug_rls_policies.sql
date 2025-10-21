99604447bff6813ee25af4f059a602ade432b601
유주섭 <noname@nonameui-MacBookAir.local>
2025-10-21 14:18:44 +0900

    Remove debug SQL files and add to .gitignore

--- --experimental-app/debug_rls_policies.sql
+++
@@ -1,5 +0,0 @@
--- 현재 RLS 정책 확인
-SELECT schemaname, tablename, policyname, permissive, roles, cmd, qual, with_check
-FROM pg_policies 
-WHERE tablename IN ('destinations', 'reviews', 'trip', 'profiles')
-ORDER BY tablename, policyname;
